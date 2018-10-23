4925414c67e34a1c1c48b3edda77f384c1d0946e
XinZhang <zhanxin_chn@126.com>
2018-10-23 23:33:08 +0800

    fix

--- test_script_4.py
+++ test_script_4.py
@@ -4,43 +4,19 @@
 # @File    : test_script_4.py
 
 # --------------------------------------------------------------------------
-from option_tools.option_pricer.options.engine_types import (
-    EngineType, OptionType, ExerciseType, AveragingContinuity, AsianAverageType)
+
+from option_tools.option_pricer.options.option_base import EuropeanOption, AmericanOption, AsianOption, AmericanAsianOption, BarrierOptionBase
 
 
-mc = (
-    OptionType.CALL | ExerciseType.EUROPEAN | AsianAverageType.ARITHMETIC,
-    OptionType.PUT | ExerciseType.EUROPEAN | AsianAverageType.ARITHMETIC,
-)
+print(EuropeanOption.oid)
+print(AmericanOption.oid)
 
-fd = (
-    OptionType.CALL | ExerciseType.AMERICAN,
-    OptionType.PUT | ExerciseType.AMERICAN,
-    OptionType.CALL | ExerciseType.EUROPEAN | AsianAverageType.ARITHMETIC | AveragingContinuity.DISCRETE,
-    OptionType.PUT | ExerciseType.EUROPEAN | AsianAverageType.ARITHMETIC | AveragingContinuity.DISCRETE,
-)
-
-ana = (
-    OptionType.CALL | ExerciseType.EUROPEAN,
-    OptionType.PUT | ExerciseType.EUROPEAN,
-)
+print(AsianOption.oid)
+print(BarrierOptionBase.oid)
+print(AmericanAsianOption.oid)
 
 
-for m in mc:
-    print(m | EngineType.MC)
 
-for f in fd:
-    print(f | EngineType.FD)
-
-for a in ana:
-    print(a | EngineType.ANALYTIC)
-
-for a in ana:
-    print(a | EngineType.MC)
-
-# --------------------------------------------------------------------------
-from option_tools.option_pricer.options.option_base import EuropeanOption
-from option_tools.option_pricer.options.option_types import OptionType
 
 """
 European call option
